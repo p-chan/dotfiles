7230feca51c4395d928b5bf7d78f727f6e47fa09
P-Chan <hello@0x50.io>
2016-12-20 07:04:35 +0900

    [hyper] Add predawn-hyperterm
    Change design from default to predawn

--- .hyper.js
+++ .hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
@@ -91,7 +91,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'predawn-hyperterm'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
