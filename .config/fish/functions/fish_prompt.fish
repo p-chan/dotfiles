a16f5adc08adff48fb67972c3f1fe54dec43a29c
P-Chan <hello@0x50.io>
2017-02-09 09:12:05 +0900

    [fish] Add git branch to prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt
-  printf '%s %s ' (set_color blue)(echo '⋊>') (set_color bryellow)(prompt_pwd)
+  if git_is_repo
+    printf '%s %s %s %s %s ' (set_color blue)(echo '⋊>') (set_color bryellow)(prompt_pwd) (set_color white)(echo 'on') (set_color green)(git_is_branch) (set_color white)(echo '○')(set_color white)
+  else
+    printf '%s %s ' (set_color blue)(echo '⋊>') (set_color bryellow)(prompt_pwd)(set_color white)
+  end
 end
