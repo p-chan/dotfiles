ac3dd259bec1f09ac721ec1adc75f63d05e9d8b3
P-Chan <hello@0x50.io>
2018-07-16 08:20:10 +0900

    feat(fish, node): Add install script

---
+++ .config/fish/functions/node_install.fish
@@ -0,0 +1,11 @@
+function node_install
+  set -l use_node_version 8
+
+  if not test nodebrew
+    printf "[error] nodebrew is required"
+    return $status
+  end
+
+  nodebrew install-binary v$use_node_version
+  nodebrew use v$use_node_version
+end
