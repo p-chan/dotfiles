1b733ce56a19706a8b12dbdf1dc0a7dca3ed1cb5
P-Chan <hello@0x50.io>
2022-01-07 18:09:09 +0900

    fix: fix fish prompt

--- .config/fish/functions/git_is_repo.fish
+++ .config/fish/functions/git_is_repo.fish
@@ -1,5 +1,5 @@
 function git_is_repo
-    if not command git rev-parse --is-inside-work-tree >/dev/null ^/dev/null
+    if not command git rev-parse --is-inside-work-tree &>/dev/null
         return 1
     end
 end
