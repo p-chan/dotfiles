63735d6f672814314989c0ce6859800204c0c22b
P-Chan <hello@0x50.io>
2022-01-09 01:11:29 +0900

    feat: change Makefile to link.sh

---
+++ link.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIRNAME=$(cd $(dirname $0) && pwd)
+
+if [ ! -d $HOME/.config ]; then
+  mkdir $HOME/.config
+fi
+
+rm -rf $HOME/.config/fish
+ln -sfnv $DIRNAME/.config/fish $HOME/.config/fish
+
+rm -rf $HOME/.config/karabiner
+ln -sfnv $DIRNAME/.config/karabiner $HOME/.config/karabiner
+
+rm -rf $HOME/.config/nodenv
+ln -sfnv $DIRNAME/.config/nodenv $HOME/.config/nodenv
+
+if [ ! -d $HOME/.ssh ]; then
+  ln -sfnv $DIRNAME/.ssh $HOME/.ssh
+fi
+
+rm -rf $HOME/.vim
+ln -sfnv $DIRNAME/.vim $HOME/.vim
+
+ln -sfnv $DIRNAME/.editorconfig $HOME/.editorconfig
+ln -sfnv $DIRNAME/.fixpackrc $HOME/.fixpackrc
+ln -sfnv $DIRNAME/.gitconfig $HOME/.gitconfig
+ln -sfnv $DIRNAME/.gitexclude $HOME/.gitexclude
+ln -sfnv $DIRNAME/.hyper.js $HOME/.hyper.js
