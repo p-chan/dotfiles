0b3e1e3ac48af73173e92fe5a33e2e20b575ee90
P-Chan <hello@0x50.io>
2025-05-04 16:36:59 +0900

    feat(zsh): add zsh (#29)

--- link.sh
+++ link.sh
@@ -29,3 +29,6 @@ ln -sfnv $DIRNAME/home/.ssh $HOME/.ssh
 
 ln -sfnv $DIRNAME/home/.editorconfig $HOME/.editorconfig
 ln -sfnv $DIRNAME/home/.hyper.js $HOME/.hyper.js
+ln -sfnv $DIRNAME/home/.zprofile $HOME/.zprofile
+ln -sfnv $DIRNAME/home/.zshenv $HOME/.zshenv
+ln -sfnv $DIRNAME/home/.zshrc $HOME/.zshrc
