8d01e4dd77236d145ce5c5b5a19c27ccac35fd79
P-Chan <hello@0x50.io>
2025-05-04 13:10:20 +0900

    refactor: move linkable files to home directory (#28)

--- link.sh
+++ link.sh
@@ -7,25 +7,25 @@ if [ ! -d $HOME/.config ]; then
 fi
 
 rm -rf $HOME/.config/fish
-ln -sfnv $DIRNAME/.config/fish $HOME/.config/fish
+ln -sfnv $DIRNAME/home/.config/fish $HOME/.config/fish
 
 rm -rf $HOME/.config/fixpack
-ln -sfnv $DIRNAME/.config/fixpack $HOME/.config/fixpack
+ln -sfnv $DIRNAME/home/.config/fixpack $HOME/.config/fixpack
 
 rm -rf $HOME/.config/git
-ln -sfnv $DIRNAME/.config/git $HOME/.config/git
+ln -sfnv $DIRNAME/home/.config/git $HOME/.config/git
 
 rm -rf $HOME/.config/karabiner
-ln -sfnv $DIRNAME/.config/karabiner $HOME/.config/karabiner
+ln -sfnv $DIRNAME/home/.config/karabiner $HOME/.config/karabiner
 
 rm -rf $HOME/.config/nodenv
-ln -sfnv $DIRNAME/.config/nodenv $HOME/.config/nodenv
+ln -sfnv $DIRNAME/home/.config/nodenv $HOME/.config/nodenv
 
 rm -rf $HOME/.config/vim
-ln -sfnv $DIRNAME/.config/vim $HOME/.config/vim
+ln -sfnv $DIRNAME/home/.config/vim $HOME/.config/vim
 
 rm -rf $HOME/.ssh
-ln -sfnv $DIRNAME/.ssh $HOME/.ssh
+ln -sfnv $DIRNAME/home/.ssh $HOME/.ssh
 
-ln -sfnv $DIRNAME/.editorconfig $HOME/.editorconfig
-ln -sfnv $DIRNAME/.hyper.js $HOME/.hyper.js
+ln -sfnv $DIRNAME/home/.editorconfig $HOME/.editorconfig
+ln -sfnv $DIRNAME/home/.hyper.js $HOME/.hyper.js
