22c097086701b2a4a5d808303091a2f1802a76ce
P-Chan <hello@0x50.io>
2025-05-12 01:57:49 +0900

    feat(starship): add config

--- scripts/link.sh
+++ scripts/link.sh
@@ -9,6 +9,7 @@ dotfiles=(
   ".config/karabiner"
   ".config/mise"
   ".config/vim"
+  ".config/starship.toml"
   ".ssh"
   "Library/Application Support/Code/User/keybindings.json"
   "Library/Application Support/Code/User/settings.json"
