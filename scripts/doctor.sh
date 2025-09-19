0099505b31173467a29f88d39e3647e91d660c2b
P-Chan <hello@0x50.io>
2025-09-19 20:19:18 +0900

    chore: add GitHub Codespaces support (#175)
    * chore: add GitHub Codespaces support
    
    * ci: fix ci
    
    * ci: tweaks
    
    * ci: tweaks
    
    * chore: add zsh install step
    
    * feat: add VSCode settings symbolic link for Ubuntu
    
    * chore: add Ubuntu support to `link.sh`
    
    * ci: add `-x` option to shellcheck
    
    * chore: add comments for shellcheck
    
    * feat: fix defautl shell to zsh

--- scripts/doctor.sh
+++ scripts/doctor.sh
@@ -2,24 +2,12 @@
 
 set -e
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=scripts/utils.sh
+source "$SCRIPT_DIR/utils.sh"
+
 required_commands=("zsh" "vim" "git" "mise" "deno" "go" "node" "rustc")
 darwin_required_commands=("xcode-select" "brew")
-
-is_darwin() {
-  [[ "$(uname)" == "Darwin" ]]
-}
-
-log_info () {
-  echo "ℹ️ $1"
-}
-
-log_error () {
-  echo "❌ $1"
-}
-
-log_success () {
-  echo "✅ $1"
-}
 
 log_info "Checking required commands..."
 
