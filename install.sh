22a4207d50ac010c04b1331a0d9b0cfc4d03d00b
P-Chan <hello@0x50.io>
2025-09-28 05:08:30 +0900

    refactor: refactor `DOTFILES_DIR` variable

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-DOTFILES_DIR="/workspaces/.codespaces/.persistedshare/dotfiles" bash scripts/install.sh
+DOTFILES_DIR="/workspaces/.codespaces/.persistedshare/dotfiles"
+
+DOTFILES_DIR="$DOTFILES_DIR" bash scripts/install.sh
+
+echo $DOTFILES_DIR >> "$HOME/.config/dotfiles/dotfiles-dir"
