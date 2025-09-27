d1fc12d0fd6996df2b082ffc8d5a4c83e26b9797
P-Chan <hello@0x50.io>
2025-09-28 05:13:05 +0900

    fix: add mkdir commands

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ DOTFILES_DIR="/workspaces/.codespaces/.persistedshare/dotfiles"
 
 DOTFILES_DIR="$DOTFILES_DIR" bash scripts/install.sh
 
+mkdir -p "$HOME/.config/dotfiles"
+
 echo $DOTFILES_DIR >> "$HOME/.config/dotfiles/dotfiles-dir"
