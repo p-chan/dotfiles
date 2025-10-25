931b52cf03839e0cd7d2ed78fc6ef34b3d6f2f7d
P-Chan <hello@0x50.io>
2025-10-25 14:41:33 +0900

    feat: add exist check

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -77,14 +77,14 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
 # Arrange Dock
 dockutil --remove all
-dockutil --add "/Applications/Fantastical.app"
-dockutil --add "/Applications/Mimestream.app"
-dockutil --add "/Applications/Arc.app"
-dockutil --add "/Applications/Slack.app"
-dockutil --add "/Applications/Figma.app"
-dockutil --add "/Applications/1Password.app"
-dockutil --add "/Applications/Visual Studio Code.app"
-dockutil --add "/Applications/Ghostty.app"
+[ -d "/Applications/Fantastical.app" ] && dockutil --add "/Applications/Fantastical.app"
+[ -d "/Applications/Mimestream.app" ] && dockutil --add "/Applications/Mimestream.app"
+[ -d "/Applications/Arc.app" ] && dockutil --add "/Applications/Arc.app"
+[ -d "/Applications/Slack.app" ] && dockutil --add "/Applications/Slack.app"
+[ -d "/Applications/Figma.app" ] && dockutil --add "/Applications/Figma.app"
+[ -d "/Applications/1Password.app" ] && dockutil --add "/Applications/1Password.app"
+[ -d "/Applications/Visual Studio Code.app" ] && dockutil --add "/Applications/Visual Studio Code.app"
+[ -d "/Applications/Ghostty.app" ] && dockutil --add "/Applications/Ghostty.app"
 
 # Unhide Library directory
 chflags nohidden ~/Library
